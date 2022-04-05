245e556ed157aded1066398a7d1f09f270d55a0f
abramtan <72917689+abramtan@users.noreply.github.com>
2022-04-05 15:35:36 +0800

    Added 'term' spinner

--- app/src/main/java/com/example/sutdfolio/Upload.java
+++ app/src/main/java/com/example/sutdfolio/Upload.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Upload extends Fragment {
 
@@ -23,7 +25,14 @@ public class Upload extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.upload_fragment, container, false);
+        View v = inflater.inflate(R.layout.upload_fragment, container, false);
+
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinnerTerm);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.term_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        return v;
     }
 
     @Override
